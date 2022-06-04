4c6a1b269199cfdd76f938d10fed5eff020a1b96
Emmanuel Effiong <etokoffice@gmail.com>
2022-06-04 23:06:28 +0000

    0-print_list_integer.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+for element_at(my_list, idx):
+    """retrives an element from a list at a specific index"""
+    if idx < 0 or idx > len(my_list) - 1:
+        return None
+    else:
+        return my_list[idx]
