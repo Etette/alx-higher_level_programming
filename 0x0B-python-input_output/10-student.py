15f2dbce1751684bf50526e8745f095af730a854
Emmanuel Effiong <etokoffice@gmail.com>
2022-07-06 20:04:30 +0000

    added all task

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+json
+"""
+
+
+class Student:
+    """ A student class"""
+    def __init__(self, first_name, last_name, age):
+        """initialize student class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves dict rep of student instance"""
+        if attrs is not None and all(isinstance(x, str) for x in attrs):
+            d = {}
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    d[k] = v
+            return d
+        else:
+            return self.__dict__
