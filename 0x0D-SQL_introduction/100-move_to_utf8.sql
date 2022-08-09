27220b842e1f1d8bb34aec895b18835646554041
Emmanuel Effiong <etokoffice@gmail.com>
2022-08-09 23:52:31 +0000

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,13 +7,13 @@ CHARACTER SET utf8mb4
 COLLATE utf8mb4_uncode_ci
 
 -- convert first_table
-USE hbtn_0c_0
+USE hbtn_0c_0;
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci
 
 -- convert field 'name' in 'first_table'
-USE hbtn_0c_0
+USE hbtn_0c_0;
 ALTER TABLE first_table
 CHANGE name name VARCHAR(256)
 CHARACTER SET utf8mb4
