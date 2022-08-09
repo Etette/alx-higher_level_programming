fc349d6518b1009809d88abda491de905c57205c
Emmanuel Effiong <etokoffice@gmail.com>
2022-08-09 22:10:17 +0000

    fixed

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Write a Script that creates the database 'hbtn_0c_0' in MySQL server
 -- if database does not exist, script should not fail
-CREATE DATABASE IF NOT EXISTS htbn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Write a script that creates a tables in current database
 -- description: id INT, name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
