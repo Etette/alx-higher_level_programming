298bc773f35c021367743692b918dd001667599d
Emmanuel Effiong <etokoffice@gmail.com>
2022-08-09 23:53:37 +0000

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,8 +3,8 @@
 
 -- convert db hbtn_0c_0
 ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_uncode_ci
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_uncode_ci
 
 -- convert first_table
 USE hbtn_0c_0;
