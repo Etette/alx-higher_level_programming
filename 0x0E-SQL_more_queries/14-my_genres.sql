cf96b00bbfc67beacc6e49995833e479e013f21c
Emmanuel Effiong <etokoffice@gmail.com>
2022-08-10 22:11:26 +0000

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,9 @@
 -- List all genres in DEXTER
 --Display tv_genres.name in ascending order
-SELECT tv_genres.name
+SELECT name
 FROM tv_genres
-INNER JOIN tv_show_genres m ON tv_genres.id = m.genre_id
-INNER JOIN tv_shows s ON m.show_id = s.id
-WHERE s.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+GROUP BY name
+ORDER BY name ASC;
