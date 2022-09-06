f77953163116f2d502e9841634124e2f2d246048
Etette Etok <emmanueletok5@gmail.com>
2022-09-06 17:17:54 +0000

    fiexed errors

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ const numTimes = parseInt(process.argv[2]);
 if (isNaN(numTimes) || numTimes === undefined) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < numTimes.length; i++) {
+  for (let i = 0; i < numTimes; i++) {
     console.log('C is fun');
   }
 }
